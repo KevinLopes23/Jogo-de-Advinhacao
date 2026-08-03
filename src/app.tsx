da7bfdab7fbf54a8ffcbf0380cca2703cd4f181c
Kevin Lopes <kevinlopes2304@gmail.com>
2026-08-03 18:09:26 -0300

    Refactor App component to use modular CSS and include Header component; remove unused App.css file.

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,12 @@
-export default function App(){
+import styles from "./app.module.css";
+import { Header } from "./components/Header";
+
+export default function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className={styles.container}>
+      <main>
+        <Header />
+      </main>
     </div>
-  )
+  );
 }
