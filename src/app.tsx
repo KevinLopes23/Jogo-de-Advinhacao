64cfe4c54b08910e5335beed7f9f03071dc4025d
Kevin Lopes <kevinlopes2304@gmail.com>
2026-08-04 14:11:33 -0300

    Add Letter component to App for displaying letters in a word; update styles for container and word layout.

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import styles from "./app.module.css";
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
 
 export default function App() {
   function handleRestartGame() {
@@ -12,6 +13,14 @@ export default function App() {
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
         <Tip tip="Uma das linguagens de programacao mais utilizadas" />
+
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
       </main>
     </div>
   );
