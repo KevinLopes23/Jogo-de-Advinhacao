9acdcaa5128e1e56a6ab4782624262f37e6e4100
Kevin Lopes <kevinlopes2304@gmail.com>
2026-08-19 15:53:02 -0300

    Enhance game restart functionality in App component by adding confirmation dialog before restarting the game.

--- src/app.tsx
+++ src/app.tsx
@@ -17,7 +17,11 @@ export default function App() {
   const [challenge, setChallenge] = useState<Challenge | null>(null);
 
   function handleRestartGame() {
-    alert("Reiniciar o jogo");
+    const isConfirmed = window.confirm("Voce tem certeza que deseja reiniciar");
+
+    if (isConfirmed) {
+      startGame();
+    }
   }
 
   function startGame() {
